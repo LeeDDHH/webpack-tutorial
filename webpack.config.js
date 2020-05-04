7ab05750c34f2508aabe93d381ca9f93088ebab0
dongheon lee <niche3600@gmail.com>
2020-05-04 15:47:17 +0900

    generate seperate CSS and auto-generate HTML

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');// cssを別ファイルとして吐き出す
+const HtmlWebpackPlugin = require('html-webpack-plugin');//htmlを自動で生成したり、指定したhtmlをテンプレートにwebpackを通してjs,cssが読み込まれるようにする
 
 module.exports = {
   entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, './dist/'),
-    filename: 'main.js'
+    path: path.resolve(__dirname, "./dist/"),
+    filename: "main.js",
   },
   module: {
     rules: [
@@ -12,13 +14,19 @@ module.exports = {
         test: /\.css/,
         use: [
           {
-            loader: 'style-loader'
+            loader: MiniCssExtractPlugin.loader
           },
           {
-            loader: 'css-loader'
-          }
-        ]
-      }
-    ]
-  }
-}
+            loader: "css-loader",
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ],
+};
