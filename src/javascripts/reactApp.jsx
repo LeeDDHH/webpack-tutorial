20ffd4737c78b55b750ee4d2524d8546ec359d32
dongheon lee <niche3600@gmail.com>
2020-05-06 13:13:02 +0900

    add styled-components library

--- src/javascripts/reactApp.jsx
+++ src/javascripts/reactApp.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import styled from 'styled-components'
+
+const message = 'カモの自己紹介ページ';
+
+const AppContainer = styled.div`
+  background-color: gold;
+  color: #696969;
+  padding: 1em;
+  border-radius: 1em;
+`;
 
 const App = (props) => {
   return (
-    <div>
-      reactapp
-    </div>
+    <AppContainer>
+      {message}
+    </AppContainer>
   );
 };
 
